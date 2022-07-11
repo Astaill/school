b0469f5a79ab042098958309ebe17371d7c5c6b8
Astaill <108857985+Astaill@users.noreply.github.com>
2022-07-11 14:57:29 +0800

    new

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():  # put application's code here
+    return render_template('index.html')
+
+
+@app.route('/Data-Exploration.html')
+def DataExploration():
+    return render_template('Data-Exploration.html')
+
+
+
+@app.route('/Data-Preparation.html')
+def DataPreparation():
+    return render_template('Data-Preparation.html')
+
+
+@app.route('/Evaluation.html')
+def Evaluation():
+    return render_template('Evaluation.html')
+
+
+@app.route('/Conclusion.html')
+def Conclusion():
+    return render_template('Conclusion.html')
+
+
+@app.route('/Contact.html')
+def Contact():
+    return render_template('Contact.html')
+
+
+@app.route('/Introduction.html')
+def Introduction():
+    return render_template('Introduction.html')
+
+if __name__=="__main__":
+    app.run(debug=True)
